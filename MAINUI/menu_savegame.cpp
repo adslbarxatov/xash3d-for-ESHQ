6c93b54c0e8b289fe6527fdd2fc6a90a6379abf6
Nicolay B. aka RD_AAOW <adslbarxatov@mail.ru>
2021-04-15 15:51:06 +1100

    Update menu_savegame.cpp

--- MAINUI/menu_savegame.cpp
+++ MAINUI/menu_savegame.cpp
@@ -298,11 +298,11 @@ static void UI_SaveGame_Ownerdraw( void *self )
 
 			sprintf( saveshot, "save/%s.bmp", uiSaveGame.saveName[uiSaveGame.savesList.curItem] );
 
-			if( !FILE_EXISTS( saveshot ))
-				UI_DrawPicAdditive( x, y, w, h, uiColorWhite, "{GRAF001" );
+			if( !g_engfuncs.pfnFileExists( saveshot, TRUE ))
+				UI_DrawPicAdditive( x, y, w, h, uiColorWhite, EMPTY_SAVE_PIC );
 			else UI_DrawPic( x, y, w, h, uiColorWhite, saveshot );
 		}
-		else UI_DrawPicAdditive( x, y, w, h, uiColorWhite, "{GRAF001" );
+		else UI_DrawPicAdditive( x, y, w, h, uiColorWhite, EMPTY_SAVE_PIC );
 
 		// draw the rectangle
 		UI_DrawRectangle( item->x, item->y, item->width, item->height, uiInputFgColor );
