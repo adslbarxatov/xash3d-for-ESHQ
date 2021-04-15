1521a20ade7d172d20869e3319011a7cb7f08b59
Nicolay B. aka RD_AAOW <adslbarxatov@mail.ru>
2021-04-15 14:00:46 +1100

    Update voice_status.h

--- game_shared/voice_status.h
+++ game_shared/voice_status.h
@@ -192,7 +192,7 @@ public:
 	// Squelch mode stuff.
 	bool				m_bInSquelchMode;
 	
-	HLSPRITE				m_VoiceHeadModel;		// Voice head model (goes above players who are speaking).
+	HSPRITE				m_VoiceHeadModel;		// Voice head model (goes above players who are speaking).
 	float				m_VoiceHeadModelHeight;	// Height above their head to place the model.
 
 	vgui::Image			*m_pSpeakerLabelIcon;	// Icon next to speaker labels.
